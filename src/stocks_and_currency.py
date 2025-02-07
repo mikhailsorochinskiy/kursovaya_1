adee26f9afd35ab6235da174f1fe24e23b6b0b82
Michael <sorokajr678@bk.ru>
2025-02-07 19:33:14 +0300

    add user_settings.json

--- src/stocks_and_currency.py
+++ src/stocks_and_currency.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -7,8 +8,14 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 API_KEY_STOCKS = os.getenv("API_KEY_STOCKS")
 
+PATH_TO_FILE = os.path.dirname(os.path.dirname(__file__))
+PATH_TO_JSON_FILE = os.path.join(PATH_TO_FILE, 'user_settings.json')
+
+stock_currency_dict = dict()
+
 
 def get_currency_rate():
+    """Функция возвращает курс доллара и евро"""
     url_1 = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=1"
     url_2 = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=1"
     payload = {}
@@ -19,13 +26,18 @@ def get_currency_rate():
     response_usd = requests.request("GET", url_2, headers=headers, data=payload)
     result_eur = response_eur.json()["result"]
     result_usd = response_usd.json()["result"]
-    return {
+    result = {
       'EUR': result_eur,
       'USD': result_usd
     }
+    stock_currency_dict['currency'] = [key for key in result.keys()]
+    with open(PATH_TO_JSON_FILE, 'w+') as file:
+        json.dump(stock_currency_dict, file, indent=4)
+    return result
 
 
 def get_stocks_rate():
+    """Функция возвращает курс акций S&P500"""
     api_url_aapl = 'https://api.api-ninjas.com/v1/stockprice?ticker={}'.format('AAPL')
     response_aapl = requests.get(api_url_aapl, headers={'X-Api-Key': API_KEY_STOCKS})
     api_url_amzn = 'https://api.api-ninjas.com/v1/stockprice?ticker={}'.format('AMZN')
@@ -41,4 +53,7 @@ def get_stocks_rate():
               {'stock': 'GOOGL', 'price': response_googl.json()['price']},
               {'stock': 'MSFT', 'price': response_msft.json()['price']},
               {'stock': 'TSLA', 'price': response_tsla.json()['price']}]
+    stock_currency_dict['stocks'] = [stock['stock'] for stock in result]
+    with open(PATH_TO_JSON_FILE, 'w+') as file:
+        json.dump(stock_currency_dict, file, indent=4)
     return result
